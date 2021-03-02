90f23edc913a589040dd3b7c7890fc91faa95775
basil Korneychuk <basil.korneychuk@gmail.com>
2021-03-02 22:06:49 +0300

    piu!

--- src/main.cpp
+++ src/main.cpp
@@ -4,23 +4,33 @@
 
 
 int main(void) {
-    GLFWwindow* window;
 
     /* Initialize the library */
-    if (!glfwInit())
+    if (!glfwInit()) {
+        std::cout << "glfwInit failed" << std::endl;
         return -1;
+    }
 
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window) {
+    GLFWwindow* pWindow;
+    pWindow = glfwCreateWindow(640, 480, "Battle City", nullptr, nullptr);
+    if (!pWindow) {
+        std::cout << "glfwCreateWindow failed" << std::endl;
         glfwTerminate();
         return -1;
     }
 	
+    std::cout << "Renderer: " << glGetString(GL_RENDERER) << std::endl;
+    std::cout << "OpenGL version: " << glGetString(GL_VERSION) << std::endl;
+
 	std::cout << "open GL: " << GLVersion.major << "." << GLVersion.minor << std::endl;
 
     /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+    glfwMakeContextCurrent(pWindow);
 	
 	if(!gladLoadGL()){
 		std::cout << "Cant load GLAD" << std::endl;
@@ -30,12 +40,12 @@ int main(void) {
 	glClearColor(0, 1, 0, 1);
 
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window)) {
+    while (!glfwWindowShouldClose(pWindow)) {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
         /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+        glfwSwapBuffers(pWindow);
 
         /* Poll for and process events */
         glfwPollEvents();
